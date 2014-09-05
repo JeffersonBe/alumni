8a477128d7e042b0c59c8ecf8be9221ce93b270c
jeffersonbe <jefferson.bonnaire@gmail.com>
2014-09-05 22:15:34 +0200

    Create login

--- app/routes.php
+++ app/routes.php
@@ -16,4 +16,8 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
+// route to show the login form
+Route::get('login', array('uses' => 'HomeController@showLogin'));
+// route to process the form
+Route::post('login', array('uses' => 'HomeController@doLogin'));
 Route::get('profile', 'ProfileController@showProfile');
