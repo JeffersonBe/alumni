d954f711927779f2e905cf6060ede66965f915aa
jeffersonbe <jefferson.bonnaire@gmail.com>
2014-09-20 15:26:50 +0200

    Add Entrust (User permission management)

--- app/models/User.php
+++ app/models/User.php
@@ -2,8 +2,19 @@
 
 use Zizaco\Confide\ConfideUser;
 use Zizaco\Confide\ConfideUserInterface;
+use Zizaco\Entrust\HasRole;
 
 class User extends Eloquent implements ConfideUserInterface
 {
     use ConfideUser;
+
+// This is trait for using entrust
+    use HasRole;
+
+     /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 }
